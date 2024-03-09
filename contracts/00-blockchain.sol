bf44f209e3795642330db92ca62358d1e48877fb
adesinna <aladesaea@yahoo.com>
2024-03-09 08:31:55 +0100

    contracts

---
+++ contracts/00-blockchain.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+/* this is a comment */
+
+// this is also a comment
+
+/* 
+Smart Contracts are running on blockchain and are deployed as EVM Bytecode
+
+The are turing complere which means they can perfrom any any operations
+
+
+Solidity have familiar concepts to python
+
+Deployment of a smart contract is done with a transaction
+
+*/
